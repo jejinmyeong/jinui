efef4cb6ce8f87da2e0ed7f751195d5cefbbf240
jejinmyeong <1995jjm@naver.com>
2024-03-31 18:58:09 +0900

    env: update eslint, prettier

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,13 +3,15 @@ module.exports = {
   env: { browser: true, es2020: true },
   extends: [
     'eslint:recommended',
+    'plugin:prettier/recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:react-hooks/recommended',
   ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
+  ignorePatterns: ['dist', '.eslintrc.cjs', 'node_moduels/'],
   parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
+  plugins: ['react-refresh', 'prettier'],
   rules: {
+    'prettier/prettier': "error",
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
