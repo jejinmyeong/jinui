d4fae3a8beb1f1c0a9a6d4bb608a8a6e42cba71d
jejinmyeong <1995jjm@naver.com>
2024-04-03 00:29:58 +0900

    env: code formatting

--- vite.config.ts
+++ vite.config.ts
@@ -1,35 +1,34 @@
-import { defineConfig } from 'vite';
-import { extname, relative, resolve } from 'path';
+import {defineConfig} from 'vite';
+import {extname, relative, resolve} from 'path';
 import react from '@vitejs/plugin-react';
 import dts from 'vite-plugin-dts';
-import { libInjectCss } from 'vite-plugin-lib-inject-css';
-import { fileURLToPath } from 'node:url';
-import { glob } from 'glob';
+import {libInjectCss} from 'vite-plugin-lib-inject-css';
+import {fileURLToPath} from 'node:url';
+import {glob} from 'glob';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), libInjectCss(), dts({ include: ['lib']})],
+  plugins: [react(), libInjectCss(), dts({include: ['lib']})],
   build: {
     copyPublicDir: false,
     lib: {
       entry: resolve(__dirname, 'lib/main.ts'),
-      formats: ['es']
+      formats: ['es'],
     },
     rollupOptions: {
       external: ['react', 'react/jsx-runtime'],
       input: Object.fromEntries(
-        glob.sync('lib/**/*.{ts,tsx}', { ignore: "lib/**/*.stories.tsx"}).map(file => [
-          relative(
-            'lib',
-            file.slice(0, file.length - extname(file).length)
-          ),
-          fileURLToPath(new URL(file, import.meta.url))
-        ])
+        glob
+          .sync('lib/**/*.{ts,tsx}', {ignore: 'lib/**/*.stories.tsx'})
+          .map(file => [
+            relative('lib', file.slice(0, file.length - extname(file).length)),
+            fileURLToPath(new URL(file, import.meta.url)),
+          ]),
       ),
       output: {
         assetFileNames: 'assets/[name][extname]',
-        entryFileNames: '[name].js'
-      }
-    }
-  }
-})
+        entryFileNames: '[name].js',
+      },
+    },
+  },
+});
