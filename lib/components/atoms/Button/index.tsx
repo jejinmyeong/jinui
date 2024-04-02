3a8f5268aa8d6d6b965781fa70826c330487c70f
jejinmyeong <1995jjm@naver.com>
2024-04-03 00:30:46 +0900

    feat: button component

--- lib/components/atoms/Button/index.tsx
+++ lib/components/atoms/Button/index.tsx
@@ -4,23 +4,29 @@ import classNames from 'classnames';
 
 interface Props extends React.ButtonHTMLAttributes<HTMLButtonElement> {
   size?: 'sm' | 'md' | 'lg';
+  color?: 'primary' | 'error' | 'default';
   fullWidth?: boolean;
 }
 
 export function Button({
   className,
   size = 'md',
+  color = 'default',
   fullWidth,
   ...restProps
 }: Props) {
   return (
     <button
       className={classNames(
-        `${className} ${styles.button}`,
+        `${styles.button}`,
+        className && `${className}`,
         size === 'sm' && `${styles.sm}`,
         size === 'md' && `${styles.md}`,
         size === 'lg' && `${styles.lg}`,
         fullWidth && `${styles.fullWidth}`,
+        color === 'default' && `${styles.default}`,
+        color === 'primary' && `${styles.primary}`,
+        color === 'error' && `${styles.error}`,
       )}
       {...restProps}
     />
